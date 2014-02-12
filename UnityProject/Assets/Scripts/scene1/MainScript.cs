6679f1633937cbd7d78fa7e9324d1e828f4545f9
matmuze <lemuzimi@gmail.com>
2014-02-12 10:14:23 +0100

    Hello world

---
+++ UnityProject/Assets/Scripts/scene1/MainScript.cs
@@ -0,0 +1,162 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using System.Linq;
+using System.Collections.Generic;
+
+public class MainScript : MonoBehaviour 
+{
+	public int count = 0;
+
+	public string modeText = ""; 
+	public string parameterValueText = ""; 
+	public int stepWidth = 10; 
+	public int currParameterValue = 0; 
+
+	public delegate void AdjustParamterDelegate(int step); 
+	public AdjustParamterDelegate adjustParamFunc = null;  
+
+	private bool initiated = false;
+	private MolObject[] molObjects;
+	private MolObject focusObject; 
+
+	//static Color[] molColors = {Color.blue, Color.red, Color.yellow, Color.green, Color.cyan, Color.magenta}; 
+	// isoluminent RGB tripels from http://www.cs.ubc.ca/~tmm/courses/infovis/morereadings/FaceLumin.pdf (Figure 7)
+	static Color[] molColors = {new Color (0.847f,0.057f,0.057f), new Color(0.000f,0.592f,0.000f), new Color(0.316f,0.316f,0.991f), new Color(0.527f,0.527f,0.00f), new Color(0.000f,0.559f,0.559f), new Color(0.718f,0.000f,0.718f)}; 
+	//static Color[] molColors = {Color.blue, Color.green, Color.cyan}; 
+	//static Color[] molColors = {Color.magenta}; 
+
+	public void CreateMolObjects()
+	{
+
+		molObjects = new MolObject[count];		
+
+		for(int i = 0; i< count; i++)
+			molObjects[i] = MolObject.CreateNewMolObject(gameObject.transform, "molObject_" + i, new MolColor(molColors[UnityEngine.Random.Range(0, molColors.Count ())]));	
+
+		initiated = true;
+
+		focusObject = null; 
+	}
+
+	private Rect windowRect = new Rect(20, 20, 200, 0);
+	private const int guiTopOffset = 20;
+	private const int guiDownOffset = 10;
+	private const int guiIncrement = 15;
+	private GUIStyle style = new GUIStyle();
+
+	void OnGUI() 
+	{
+		windowRect = GUI.Window(0, windowRect, DoMyWindow, "My Window");
+	}
+
+	void DoMyWindow(int windowID) 
+	{
+		style.fontSize = 12;
+		style.normal.textColor = Color.white;
+
+		Dictionary<string, string> settingsValues = Settings.GetDictionarySettings();
+		char[] alphabet = Settings.Instance.alphabet.ToCharArray();
+
+		int count = 0;
+		foreach( KeyValuePair<string, string> kvp in settingsValues )
+		{
+			char c = alphabet[count];
+			string s = "("+ c.ToString() + ")  " + kvp.Key + ": " + kvp.Value;
+			GUI.Label(new Rect(10, guiTopOffset + count * guiIncrement, 150, 30), s, style);
+			count ++;
+		}
+		
+		windowRect.height = guiTopOffset + guiDownOffset + count * guiIncrement;
+		
+		GUI.DragWindow(new Rect(0, 0, 10000, 10000));
+	}
+
+	GameObject box;
+
+	void Start () 
+	{
+		if (!initiated) 
+		{
+			box = GameObject.Find("Box Object");
+
+			Settings.LoadSettings();
+			CreateMolObjects();
+		}
+	}
+
+	void Update () 
+	{
+		Camera.main.orthographicSize = Settings.Values.cameraSize;
+
+		box.transform.localScale = new Vector3(Settings.Values.boxSizeX, Settings.Values.boxSizeY, Settings.Values.boxSizeZ);
+
+		if (Input.GetKeyDown ("escape"))
+		{
+			if(modeText.Length == 0)
+			{
+				Application.Quit();
+			}
+			else
+			{
+				modeText = ""; 
+				parameterValueText = ""; 
+				adjustParamFunc = null; 
+			}
+		}
+
+		if (Input.GetKeyDown ("s") && Input.GetKey(KeyCode.LeftShift))
+		{
+			Settings.SaveSettings();
+		}
+
+		if (Input.GetKeyDown ("l") && Input.GetKey(KeyCode.LeftShift))
+		{
+			Settings.LoadSettings();
+		}
+
+		if (Input.GetKeyDown ("f")) {
+			InitLuminanceFlicker (GetRandomMolObject ());
+		}
+	}
+
+	void setCurrentParameter(string name, int step, AdjustParamterDelegate func)
+	{
+		modeText = name; 
+		stepWidth = step; 
+		adjustParamFunc = func; 
+		updateParameter (0); 
+	}
+
+	void updateParameter (int step)
+	{
+		if(adjustParamFunc != null){
+			adjustParamFunc(step);
+			parameterValueText = "" + currParameterValue; 
+		}
+	}
+
+	MolObject GetRandomMolObject()
+	{
+		return molObjects [UnityEngine.Random.Range (0, molObjects.Count () - 1)]; 
+	}
+
+	void InitLuminanceFlicker(MolObject molObject)
+	{
+		if (focusObject != null) 
+		{
+			focusObject.StopLuminanceFlicker (); 
+		}
+
+		molObject.StartLuminanceFlicker (); 
+		focusObject = molObject; 
+	}
+
+	void FixedUpdate()
+	{
+
+	}
+}
---
+++ UnityProject/Assets/Scripts/scene1/MolObject.cs
@@ -0,0 +1,152 @@
+﻿using UnityEngine;
+using System;
+using System.Diagnostics;
+
+public class MolObject : MonoBehaviour
+{
+	public bool animate = true;
+	public bool luminanceFlicker = false;
+
+	private MolColor defaultColor;
+	private MolColor currentColor; 
+
+	private Stopwatch stopWatch = new Stopwatch ();
+	private Stopwatch stopWatch_2 = new Stopwatch ();
+
+	public static MolObject CreateNewMolObject (Transform parent, string name, MolColor color)
+	{
+		var position = new Vector3 ((UnityEngine.Random.value - 0.5f) * Settings.Values.boxSizeX, (UnityEngine.Random.value - 0.5f) * Settings.Values.boxSizeY, (UnityEngine.Random.value - 0.5f) * Settings.Values.boxSizeZ);
+		var molGameObject = Instantiate (Resources.Load ("MolPrefab"), position, Quaternion.identity) as GameObject;
+
+		if (molGameObject != null) 
+		{
+			molGameObject.name = name;
+			molGameObject.transform.parent = parent;
+
+			var molObject = molGameObject.GetComponent<MolObject> ();
+
+			molObject.defaultColor = color; 
+			molObject.currentColor = color; 
+
+			molGameObject.GetComponent<MeshRenderer> ().material.color = color.rgba;
+
+			return molObject;
+		}
+		return null;
+	}
+
+	void Start ()
+	{
+
+	}
+
+	void FixedUpdate ()
+	{
+		if (!animate)
+			return;
+
+		rigidbody.drag = Settings.Values.drag;
+		rigidbody.AddForce (UnityEngine.Random.insideUnitSphere * Settings.Values.randomForce);
+	}
+
+	void Update ()
+	{
+		if (luminanceFlicker)
+		{
+			LuminanceFlickerUpdate (); 
+		}
+
+		transform.localScale = new Vector3(Settings.Values.molScale, Settings.Values.molScale, Settings.Values.molScale);
+
+		Vector3 temp = rigidbody.position;
+		
+		if(rigidbody.position.x > transform.parent.position.x + Settings.Values.boxSizeX * 0.5f)
+		{
+			temp.x = transform.parent.position.x + Settings.Values.boxSizeX * 0.5f - gameObject.GetComponent<SphereCollider>().radius * 2; 
+		}
+		else if(rigidbody.position.x < transform.parent.position.x - Settings.Values.boxSizeX * 0.5f)
+		{
+			temp.x = transform.parent.position.x - Settings.Values.boxSizeX * 0.5f + gameObject.GetComponent<SphereCollider>().radius * 2; 
+		}
+		
+		if(rigidbody.position.y > transform.parent.position.y + Settings.Values.boxSizeY * 0.5f)
+		{
+			temp.y = transform.parent.position.y + Settings.Values.boxSizeY * 0.5f - gameObject.GetComponent<SphereCollider>().radius * 2; 
+		}		
+		else if(rigidbody.position.y < transform.parent.position.y - Settings.Values.boxSizeY * 0.5f)
+		{
+			temp.y = transform.parent.position.y - Settings.Values.boxSizeY * 0.5f + gameObject.GetComponent<SphereCollider>().radius * 2; 
+		}
+		
+		if(rigidbody.position.z > transform.parent.position.z + Settings.Values.boxSizeZ * 0.5f)
+		{
+			temp.z = transform.parent.position.z + Settings.Values.boxSizeZ * 0.5f - gameObject.GetComponent<SphereCollider>().radius * 2; 
+		}		
+		else if(rigidbody.position.z < transform.parent.position.z - Settings.Values.boxSizeZ * 0.5f)
+		{
+			temp.z = transform.parent.position.z - Settings.Values.boxSizeZ * 0.5f + gameObject.GetComponent<SphereCollider>().radius * 2; 
+		}
+		
+		rigidbody.position = temp; 
+	}
+
+	float GetIntensityShift(float currentFrequency, float currentAmplitude, float currentTime)
+	{
+		return (float)Math.Sin(currentTime * 2.0f * (float)Math.PI * currentFrequency) * currentAmplitude;
+	}
+
+	Color ShiftColorIntensity (MolColor defaultColor, float intensity)
+	{
+		throw new NotImplementedException ();
+	}
+
+	private bool up = true;
+
+	void LuminanceFlickerUpdate()
+	{		
+		if(!stopWatch_2.IsRunning) stopWatch_2.Start();
+
+		int currentTimeMillis = (int)stopWatch.ElapsedMilliseconds;
+		float progress_1 = Mathf.Clamp((float)currentTimeMillis / Settings.Values.interpolationDuration, 0.0f, 1.0f);
+		
+		float currentHalfWaveLength = Settings.Values.startFrequency + (Settings.Values.endFrequency - Settings.Values.startFrequency) * progress_1;
+		float currentAmplitude = Settings.Values.startAmplitude + (Settings.Values.endAmplitude - Settings.Values.startAmplitude) * progress_1;		
+		
+		int currentWaveTime = (int)stopWatch_2.ElapsedMilliseconds;
+		float progress_2 = (float) currentWaveTime / currentHalfWaveLength;	
+
+		float intensityShift = Mathf.Clamp((up) ? progress_2 * 2.0f - 1.0f : (1.0f-progress_2) * 2.0f - 1.0f, -1.0f, 1.0f) * currentAmplitude;
+		float currentIntensity = Mathf.Clamp(intensityShift + Settings.Values.amplitudeOffset + defaultColor.L, 0, 100);		
+
+		currentColor = new MolColor(currentIntensity, defaultColor.a, defaultColor.b);
+		gameObject.GetComponent<MeshRenderer> ().material.color = currentColor.rgba;
+
+		if(currentWaveTime > currentHalfWaveLength)
+		{
+			up = !up;
+			stopWatch_2.Reset();
+			stopWatch_2.Start();
+		}
+
+		if(currentTimeMillis > Settings.Values.totalDuration)
+		{
+			StopLuminanceFlicker();
+		}
+	}
+
+	public void StartLuminanceFlicker()
+	{
+		luminanceFlicker = true; 
+
+		stopWatch.Start (); 
+	}
+
+	public void StopLuminanceFlicker()
+	{
+		luminanceFlicker = false; 
+		currentColor = defaultColor; 
+		gameObject.GetComponent<MeshRenderer> ().material.color = currentColor.rgba; 
+
+		stopWatch.Reset (); 
+	}
+}
