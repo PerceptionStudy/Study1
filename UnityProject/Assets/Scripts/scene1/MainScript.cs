d5e64be2fa31b81954050e3bb24e930add966af5
matmuze <lemuzimi@gmail.com>
2014-02-13 15:22:55 +0100

    Added: HUD

--- UnityProject/Assets/Scripts/scene1/MainScript.cs
+++ UnityProject/Assets/Scripts/scene1/MainScript.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using Newtonsoft.Json;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -10,6 +11,7 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text; 
+using System.Text.RegularExpressions;
 
 public class Stimulus
 {
@@ -99,26 +101,99 @@ public class MainScript : MonoBehaviour
 		stimuli = shuffle.ToList();
 	}
 
+	private bool showHud = false;
+	private const int guiTopOffset = 20;
+	private const int guiDownOffset = 10;
+	private const int guiIncrement = 15;
+	private GUIStyle style = new GUIStyle();
+	private Rect windowRect = new Rect(20, 20, 225, 0);
+	private Dictionary<string, string> tempSettings = new Dictionary<string, string>();
+
 	void OnGUI()
 	{
-		GUI.depth = 10;
+		if(showHud)
+		{
+			windowRect = GUI.Window(0, windowRect, DoMyWindow, "My Window");
+		}
 
-		if(setupGUI){
+		if(setupGUI)
+		{
 			GUI.Window (1, new Rect(0.0f, 0.0f, Screen.width, Screen.height), SetupGUI, "Setup"); 
 		}
-
-		if(intermediate){
+		
+		if(intermediate)
+		{
 			GUI.Window (2, new Rect(0.0f, 0.0f, Screen.width, Screen.height), IntermediateGUI, "Intermediate"); 
 		}
-
-		if(countdown){
+		
+		if(countdown)
+		{
 			GUI.Window (3, new Rect(0.0f, 0.0f, Screen.width, Screen.height), CountdownGUI, "Countdown"); 
 		}
-
-		if(stimulusEnd){
+		
+		if(stimulusEnd)
+		{
 			GUI.Window (4, new Rect(0.0f, 0.0f, Screen.width, Screen.height), StimulusEndGUI, "Stimulus Finished: Click where you spotted the target or press 'n' if you did not see any target"); 
 		}
 	}
+
+	void DoMyWindow(int windowID) 
+	{
+		style.fontSize = 12;
+		style.normal.textColor = Color.white;
+		
+		if(tempSettings.Count() == 0)
+		{
+			tempSettings = Settings.GetDictionarySettings();
+		}
+		
+		int count = 0;
+		
+		foreach( KeyValuePair<string, string> kvp in new Dictionary<string, string>(tempSettings) )
+		{
+			GUI.Label(new Rect(10, guiTopOffset + count * guiIncrement, 150, 30), kvp.Key + ": ", style);
+			string stringValue = GUI.TextField(new Rect(175, guiTopOffset + count * guiIncrement, 50, 20), kvp.Value.ToString(), style);
+			stringValue = Regex.Replace(stringValue, @"[^0-9.]", "");
+			
+			if(stringValue != kvp.Value)
+			{
+				tempSettings[kvp.Key] = stringValue;
+			}
+			
+			if (Event.current.isKey && Event.current.keyCode == KeyCode.Return)		
+			{				
+				float tryParse = 0.0f;
+				
+				if(float.TryParse(stringValue, out tryParse))
+				{
+					tempSettings[kvp.Key] = tryParse.ToString();
+				}
+				else
+				{
+					print("Input field parsing failed");
+					tempSettings[kvp.Key] = "0";
+				}
+			}
+			
+			count ++;
+		}
+		
+		if (Event.current.isKey && Event.current.keyCode == KeyCode.Return)		
+		{				
+			print("Applying and Saving Settings");
+			
+			string json = JsonConvert.SerializeObject(tempSettings);
+			SettingsValues v = JsonConvert.DeserializeObject<SettingsValues>(json);
+			Settings.Values = (SettingsValues)v.Clone();
+			Settings.SaveSettings();
+		}
+		
+		windowRect.height = guiTopOffset + guiDownOffset + count * guiIncrement;
+		
+		GUI.DragWindow(new Rect(0, 0, 10000, 10000));
+	}
+
+
 
 	void StimulusEndGUI(int windowID)
 	{
@@ -153,20 +228,21 @@ public class MainScript : MonoBehaviour
 
 	void SetupGUI(int windowID)
 	{
-		float xOffset = Screen.width / 3.0f + Screen.width / 12.0f; 
-		float yOffset = Screen.height / 3.0f; 
 		const float elemWidth = 150.0f; 
 		const float elemHeight = 20.0f; 
+
+		float xOffset = (Screen.width / 2.0f) - 150; 
+		float yOffset = (Screen.height / 2.0f) - 50; 
 
 		GUI.DrawTexture (new Rect (0.0f, 0.0f, Screen.width, Screen.height), (UnityEngine.Texture)Resources.Load ("guiBG")); 
 
 		GUI.Label (new Rect (xOffset, yOffset, elemWidth, elemHeight), "User ID: "); 
-		userID = GUI.TextField (new Rect (xOffset + elemWidth, yOffset, elemWidth, elemHeight), userID); 
+		userID = GUI.TextField (new Rect (xOffset + 100, yOffset, elemWidth, elemHeight), userID); 
 
-		GUI.Label (new Rect (xOffset, yOffset + elemHeight, elemWidth, elemHeight), "Condition: "); 
-		conditionID = GUI.TextField (new Rect (xOffset + elemWidth, yOffset + elemHeight, elemWidth, elemHeight), conditionID); 
+		GUI.Label (new Rect (xOffset, yOffset + elemHeight + 5, elemWidth, elemHeight), "Condition: "); 
+		conditionID = GUI.TextField (new Rect (xOffset + 100, yOffset + elemHeight + 5, elemWidth, elemHeight), conditionID); 
 
-		if(GUI.Button (new Rect(xOffset + elemWidth, yOffset + 2 * elemHeight, elemWidth, elemHeight), "Start!"))
+		if(GUI.Button (new Rect(xOffset + 100, yOffset + 2 * elemHeight + 10, elemWidth, elemHeight), "Start!"))
 		{
 			if(userID.Length > 0)
 			{
@@ -292,12 +368,17 @@ public class MainScript : MonoBehaviour
 
 		}
 
-		if (Input.GetKeyDown ("escape"))
+		if (Input.GetKeyDown (KeyCode.H))
+		{
+			showHud = !showHud;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Escape))
 		{
 			Application.Quit();
 		}
 
-		if(Input.GetKeyDown ("return"))
+		if(Input.GetKeyDown (KeyCode.Return))
 		{
 			if(intermediate)
 			{
